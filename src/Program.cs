dc6e5055849b5f471e097fbd89176b9148c3f6a5
VitasCiabis <vitas.ciabis@hospitalitaliano.org.ar>
2022-11-08 18:56:16 -0300

    .

---
+++ src/Program.cs
@@ -0,0 +1,49 @@
+﻿// CREAR PACIENTE
+/*
+namespace MyFirstFhirClient
+{
+    using System;
+    using System.Collections.Generic;
+    using Hl7.Fhir.Model;
+    using Hl7.Fhir.Rest;
+    using Hl7.Fhir.Serialization;
+    /// <summary>
+    /// Main program
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creacion de paciente Fhir");
+
+            var patient = new Patient()
+            {
+                Name = new List<HumanName>()
+                {
+                    new HumanName()
+                    {
+                        Given = new List<string>{"Mario","Victoria" },
+                        Family = "Anconetani"
+                    }
+                },
+                Gender = AdministrativeGender.Female,
+                BirthDate = "2001-09-25",
+                Identifier = new List<Identifier>()
+                {
+                    new Identifier()
+                    {
+                        Value = "43798506"
+                    }
+                }
+            };
+            Console.WriteLine($"Sending patient {patient.Name[0].Given.FirstOrDefault()} {patient.Name[0].Family} ....");
+
+            var client = new FhirClient("http://hapi.fhir.org/baseR4/");
+            client.Create(patient);
+
+            Console.WriteLine($"Result: {client.LastResult.Status}");
+        }
+    }
+}
+
+*/
---
+++ src/Program2.cs
@@ -0,0 +1,57 @@
+﻿// BUSCAR PACIENTES SEGUN SU NOMBRE
+
+namespace FHIR_TEST
+{
+    using System;
+    using System.Collections.Generic;
+    using Hl7.Fhir.Model;
+    using Hl7.Fhir.Rest;
+    using Hl7.Fhir.Serialization;
+    /// <summary>
+    /// Main program
+    /// </summary>
+
+    public static class Program
+    {
+        private const string _fhirServer = "http://hapi.fhir.org/baseR4/"; // "http://vonk.fire.ly";
+
+        static void Main(string[] args)
+        {
+            FhirClient fhirClient = new FhirClient(_fhirServer)
+            {
+                PreferredFormat = ResourceFormat.Json,
+                PreferredReturn = Prefer.ReturnRepresentation
+            };
+
+            //Bundle patientBundle = fhirClient.Search<Patient>(new string[] {"name=maria"}); //Buscar por nombre del paciente (sin importar tildes y mayus)
+            //Bundle patientBundle = fhirClient.Search<Patient>(new string[] {"identifier=43798506"}); //Buscar por DNI del paciente
+            //Bundle patientBundle = fhirClient.Search<Patient>(new string[] {"family=anconetani"}); //Buscar por apellido del paciente
+            //Bundle patientBundle = fhirClient.Search<Patient>(new string[] {"family=anconetani", "name=mario"}); //Buscar por nombre y apellido
+            Bundle patientBundle = fhirClient.Search<Patient>(new string[] {"name=maria","family=anconetani", "identifier=43798505"}); //Buscar por nombre, apellido y DNI
+
+            Console.WriteLine($"Total: {patientBundle.Total} Entry count: {patientBundle.Entry.Count}");
+
+            int patientNumber = 0;
+
+            foreach (Bundle.EntryComponent entry in patientBundle.Entry)
+            {
+                Console.WriteLine($"- Entry {patientNumber,3}: {entry.FullUrl}");
+
+                if (entry.Resource != null)
+                {
+                    Patient patient = (Patient)entry.Resource;
+                    Console.WriteLine($"- Id: {patient.Id,20}");
+
+                    if (patient.Name.Count > 0)
+                    {
+                        Console.WriteLine($" - Name: {patient.Name[0].ToString()}");
+                    }
+
+                }
+                
+                patientNumber++;
+            }
+
+        }
+    }
+}
